5ae21262d91b076d9afd0bf98aed25d07b65e689
Yogesh Gupta <yogesh.gupta38@gmail.com>
2021-09-22 19:01:56 +0530

    html done

---
+++ index.js
@@ -0,0 +1,11 @@
+const date = document.querySelector("#date-input");
+const userNumber = document.querySelector("number-entered");
+const checkNum = document.querySelector("#check-number");
+const display = document.querySelector("#display-message");
+
+
+checkNum.addEventListener("click",function calculate(){
+    console.log(date.value);
+    const DOB = 
+
+})
