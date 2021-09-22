b1e28e6e125c75daf3b2a03df4614b3a8272fd61
Yogesh Gupta <yogesh.gupta38@gmail.com>
2021-09-22 23:36:01 +0530

    done,app working

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 const date = document.querySelector("#date-input");
-const userNumber = document.querySelector("number-entered");
+const userNumber = document.querySelector("#number-entered");
 const checkNum = document.querySelector("#check-number");
 const display = document.querySelector("#display-message");
 
 
-checkNum.addEventListener("click",function calculate(){
-    console.log(date.value);
-    const DOB = 
+checkNum.addEventListener("click",calculateIfLucky);
 
-})
+function calculateIfLucky(){
+    console.log(date.value);
+    const DOB = date.value;
+    const numb = userNumber.value;
+    const sum = calculateSum(DOB);
+    getMessage(sum,numb);
+
+}
+
+function calculateSum(DOB){
+    DOB = DOB.replaceAll("-","");
+    console.log(DOB);
+    let sum = 0;
+    for (i=0;i<DOB.length;i++){
+        sum = sum +  Number(DOB.charAt(i));
+    }
+    return sum;
+}
+
+function getMessage(sum,numb){
+    console.log(sum,numb);
+    if(sum%numb===0){
+        display.innerText="You are a lucky chap";
+    }
+    else{
+        display.innerText="Your birthdate is not lucky";
+    }
+}
